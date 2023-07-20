4f800e710040d5226eeb47d53166406bd8cdebe1
ericnoharaleclair <eric.noharaleclair@gmail.com>
2023-07-20 15:17:27 -0500

    added initial html and css for main portions of application, and added event handlers for opening and closing the form

---
+++ script.js
@@ -0,0 +1,24 @@
+const formContainer = document.querySelector(".form-container");
+const listContainer = document.querySelector(".list-container");
+const exitBtn = document.querySelector(".btn-exit");
+const openFormBtn = document.querySelector(".btn-open-form");
+
+const toggleHidden = function (e) {
+  //toggle hidden class after the animation
+  setTimeout(() => listContainer.classList.toggle("hidden"), 300);
+  setTimeout(() => formContainer.classList.toggle("hidden"), 300);
+
+  //toggle animation based on which button was pressed
+  if (e.target === exitBtn) {
+    formContainer.classList.toggle("hidden-transition");
+    setTimeout(() => formContainer.classList.toggle("hidden-transition"), 400);
+  }
+  if (e.target === openFormBtn) {
+    listContainer.classList.toggle("hidden-transition");
+    setTimeout(() => listContainer.classList.toggle("hidden-transition"), 400);
+  }
+};
+
+openFormBtn.addEventListener("click", toggleHidden);
+
+exitBtn.addEventListener("click", toggleHidden);
